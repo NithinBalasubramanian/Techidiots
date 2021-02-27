efb64c03b8bec66cbd6c322ca5bb3f5644649fdf
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 15:03:39 +0530

    controller

---
+++ src/contentManage.js
@@ -0,0 +1,12 @@
+impot React from 'react';
+import './style.scss';
+
+const ContentManager = () => {
+  return(
+    <div>
+      <h1>Content</h1>
+    </div>
+  )
+}
+
+export default ContentManager;
