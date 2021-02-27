875d6372e5ad7d32bf75980a7265bb478400d1e2
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 15:19:21 +0530

    Update footer.js

---
+++ src/includes/footer.js
@@ -0,0 +1,14 @@
+impot React from 'react';
+import './style.scss';
+
+const Footer = () => {
+  return(
+    <footer>
+      <div className="footer_container">
+        <div className="copyCont">&copy; 2021 - Tech Idiots</div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
