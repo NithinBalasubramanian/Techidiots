825a83bfc51c99ec3ccec0cdaa673770ecfbf127
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 15:22:18 +0530

    bug over page content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import Header from './includes/navbar';
 import Footer from './includes/footer';
-import ContentManager from './contentManager';
+import ContentManager from './contentManage';
 
 function App() {
   return (
