6f85f2f7987544188019e2c1257f2b5c62923705
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 14:23:01 +0530

    Update navbar.js

--- src/includes/navbar.js
+++ src/includes/navbar.js
@@ -6,7 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Navbar = () => {
     return(
       <div className="NavbarMain">
-        <h1>Tech Idiots</h1> 
+        <div class="NavContainer row">
+            <div className="col-md-4">
+                <h1>Tech Idiots</h1> 
+            </div>
+        <div className="col-md-8">
+            <div className="NavList">
+                <ul className="NavListMenu">
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">BOOKS</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
       </div>
       )
 }
