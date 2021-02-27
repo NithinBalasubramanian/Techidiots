ecf39d0cb28205b16c310e518527b9d58c8650d5
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 12:07:02 +0530

    Update navbar.js

---
+++ src/includes/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Navbar = () => {
+    return(
+      <div className="NavbarMain">
+        <h1>Tech Idiots</h1> 
+      </div>
+      )
+}
+
+export default Navbar;
