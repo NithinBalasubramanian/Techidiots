c8a990a086814fb8a819a359b6bdebff7f96d4cc
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 15:06:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import Header from './includes/navbar';
+import Footer from './includes/footer';
+import ContentManager from './contentManager';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContentManager />
+      <footer />
     </div>
   );
 }
