1c7eb6d048ceb890a7dcb53a4ac077de0265ce4d
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 16:04:59 +0530

    Update contentManage.js

--- src/contentManage.js
+++ src/contentManage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.scss';
+import HomeListoutComponent from './components/own_components/homeListoutComponent';
 
 const ContentManager = () => {
   return(
     <div className="contMain">
       <h1>Content</h1>
+      <HomeListoutComponent />
     </div>
   )
 }
