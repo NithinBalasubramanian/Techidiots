9a6fe2522ff42a038903a84a04be4f458d1b922c
Nithin <nithinmigocrogle22@outlook.com>
2021-02-27 15:34:39 +0530

    Update footer.js

--- src/includes/footer.js
+++ src/includes/footer.js
@@ -2,13 +2,17 @@ import React from 'react';
 import '../style.scss';
 
 const Footer = () => {
-  return(
-    <footer>
-      <div className="footer_container">
-        <div className="copyCont">&copy; 2021 - Tech Idiots</div>
-      </div>
-    </footer>
-  )
+    let date=new Date();
+
+    let Year  = date.getFullYear();
+
+    return(
+        <footer>
+          <div className="footer_container">
+            <p className="copyCont">{ Year } - &copy; || All Right Reserved || TECH IDIOTS</p>
+          </div>
+        </footer>
+    )
 }
 
 export default Footer;
