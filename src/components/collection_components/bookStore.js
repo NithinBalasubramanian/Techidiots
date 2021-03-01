5390bf39bebdd4842db46d6c7ad6813d9d614b61
Nithin <nithinmigocrogle22@outlook.com>
2021-03-01 11:44:08 +0530

    Update bookStore.js

--- src/components/collection_components/bookStore.js
+++ src/components/collection_components/bookStore.js
@@ -37,7 +37,7 @@ const BookStore = () => {
           }
         })
         .then(response => {
-          console.log(response);
+          console.log(response.data);
         })
         .catch(err => {
           console.error(err);
