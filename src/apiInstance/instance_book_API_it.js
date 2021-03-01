a2bd301a84fa58c4a934de7c2b0a3ef466049cf1
Nithin <nithinmigocrogle22@outlook.com>
2021-03-01 12:47:18 +0530

    Create instance_book_API_it.js

---
+++ src/apiInstance/instance_book_API_it.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const Instance_API = axiosIt.create(
+    { 
+        baseURL : 'https://api.itbook.store/1.0/search/mongodb',
+    }
+)
+
+export default Instance_API;
