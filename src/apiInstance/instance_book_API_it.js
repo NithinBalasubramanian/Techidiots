c9f4ad91c178cb4e58ca90f72fca152403120c33
Nithin <nithinmigocrogle22@outlook.com>
2021-03-01 12:49:14 +0530

    Update instance_book_API_it.js

--- src/apiInstance/instance_book_API_it.js
+++ src/apiInstance/instance_book_API_it.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const Instance_API = axiosIt.create(
+const Instance_API = axios.create(
     { 
         baseURL : 'https://api.itbook.store/1.0/search/mongodb',
     }
