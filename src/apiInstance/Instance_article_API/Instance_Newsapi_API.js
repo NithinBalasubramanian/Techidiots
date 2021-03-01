4f29554bc3c41bedd1f04d5541b43c6f4ccd9098
Nithin <nithinmigocrogle22@outlook.com>
2021-03-01 14:54:49 +0530

    Create Instance_Newsapi_API.js

---
+++ src/apiInstance/Instance_article_API/Instance_Newsapi_API.js
@@ -0,0 +1,10 @@
+  
+import axios from 'axios';
+
+const Instance_Newsapi_API = axios.create(
+    { 
+        baseURL : 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=02a02f70a5ba4d0f8a5faba69b554c2e',
+    }
+)
+
+export default Instance_Newsapi_API;
