7acb1c915c91e846b488f599191f882ccd84de57
Nithin <nithinmigocrogle22@outlook.com>
2021-03-01 14:33:48 +0530

    Create articleStore.js

---
+++ src/components/collection_components/articleStore.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.scss';
+import axios from '../../apiInstance/instance_book_API';
+
+const ArticleStore = () => {
+  return(
+    <>
+      <div className="homeListContainer">
+        <h1>Article</h1>
+      </div>
+    </>
+  )
+
+}
+
+export default ArticleStore
