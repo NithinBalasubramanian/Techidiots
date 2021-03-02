7916c089e08a22c789082acb99c489e12990fbc8
Nithin <nithinmigocrogle22@outlook.com>
2021-03-02 11:12:23 +0530

    Update api_check.js

--- src/components/collection_components/api_check.js
+++ src/components/collection_components/api_check.js
@@ -2,22 +2,22 @@ import axios from "axios";
 
 
 const ApiTest = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://bing-news-search1.p.rapidapi.com/news',
-      params: {safeSearch: 'Off', textFormat: 'Raw'},
-      headers: {
-        'x-bingapis-sdk': 'true',
-        'x-rapidapi-key': '3362722dd0msh014605fac24e36bp1251bcjsnf6916b501ed2',
-        'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com'
-      }
-    };
+const options = {
+  method: 'POST',
+  url: 'https://guardianmikilior1v1.p.rapidapi.com/getTags',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'x-rapidapi-key': '3362722dd0msh014605fac24e36bp1251bcjsnf6916b501ed2',
+    'x-rapidapi-host': 'Guardianmikilior1V1.p.rapidapi.com'
+  },
+  data: {apiKey: '<REQUIRED>'}
+};
 
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
   
   
   return(
