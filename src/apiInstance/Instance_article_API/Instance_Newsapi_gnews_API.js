1e550896cb875f42e39ec1ef4e84fffc386b427c
Nithin <nithinmigocrogle22@outlook.com>
2021-03-04 11:35:07 +0530

    Create Instance_Newsapi_gnews_API.js

---
+++ src/apiInstance/Instance_article_API/Instance_Newsapi_gnews_API.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const Instance_Newsapi_API = axios.create(
+    { 
+        baseURL : 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=947rE1r1XJk0TYG0wwGlubhReDHiPvb8',
+    }
+)
+
+export default Instance_Newsapi_API;
