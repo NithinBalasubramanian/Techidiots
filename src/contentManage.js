4897500f27955dd77d7ce935e74b46e9c5bf6951
Nithin <nithinmigocrogle22@outlook.com>
2021-03-04 14:47:03 +0530

    Update contentManage.js

--- src/contentManage.js
+++ src/contentManage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 import HomeListoutComponent from './components/own_components/homeListoutComponent';
-import BlogView  from './components/own_components/homeListoutComponent';
+import BlogView  from './components/own_components/blogView';
 import BookStore from './components/collection_components/bookStore';
 import ArticleStore from './components/collection_components/articleStore';
 import ApiTest from './components/collection_components/api_check';
