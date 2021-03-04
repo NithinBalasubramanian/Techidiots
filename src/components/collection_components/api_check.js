cabe52df1dac733d6e236dcebc46aea169b986fc
Nithin <nithinmigocrogle22@outlook.com>
2021-03-04 11:23:30 +0530

    Update api_check.js

--- src/components/collection_components/api_check.js
+++ src/components/collection_components/api_check.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const ApiTest = () => {
 
-axios.get("https://gnews.io/api/v4/search?q=example&token=9f8cd16e6ad80b74e7b771a5de4be9bd").then(function (response) {
+axios.get("https://gnews.io/api/v4/top-headlines?&token=9f8cd16e6ad80b74e7b771a5de4be9bd").then(function (response) {
 	console.log(response.data);
 }).catch(function (error) {
 	console.error(error);
