143861aa9179267416c6de3f2a7917ffd2899b78
Nithin <nithinmigocrogle22@outlook.com>
2021-03-11 12:26:05 +0530

    Update blogView.js

--- src/components/own_components/blogView.js
+++ src/components/own_components/blogView.js
@@ -54,10 +54,49 @@ const BlogView = () => {
       </div>
       <div className="blog_container">
             <div className="blogContent">
-                
+                { datas.map((itm,k) => { 
+                    document.title=itm.title;
+                    return(
+                        <div>
+                        <div className="col-md-12">
+                            <h1>{itm.title}</h1>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="preheading">
+                                <p style={ {padding:'10px 0px',margin:'0px'} }>{itm.preheading}</p>
+                            </div>
+                            <div className="byAuth">
+                                - by {itm.auther} 
+                            <small>{ getDate(itm.createdOn) }</small>
+                            </div>
+                            {/* <img src={itm.filePath} alt="img" width="100%" height="auto" /> */}
+                            <img src={itm.imgUrl} alt="img" width="100%" height="auto" /> 
+                            
+                            <p>{itm.blog}</p>
+                        </div>
+                        </div>
+                    )
+                }) }
+                <div className="col-md-4 row recent">
+                    <div class="recent_part">
+                        <h4>Recent Blog</h4>
+                        {recent.map((itm,k) => {
+                            return (
+                                <div className="card_recent">
+                                    <Link to={'/Blog/'+itm.url}  >
+                                        <img src={itm.imgUrl} width="100%" height="150px" alt=""></img>
+                                        <h5>{itm.title}</h5>
+                                    </Link>
+                                </div>
+                            )
+                        })
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     </>
+    )
 }
 
 export default BlogView
