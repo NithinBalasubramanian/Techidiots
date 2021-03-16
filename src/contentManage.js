7f23ca9cf4d6681868250a6c880518a0b0116137
Nithin <nithinmigocrogle22@outlook.com>
2021-03-16 12:45:26 +0530

    Update contentManage.js

--- src/contentManage.js
+++ src/contentManage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.scss';
 import HomeListoutComponent from './components/own_components/homeListoutComponent';
 import BlogView  from './components/own_components/blogView';
+import CategoryBlogView from './components/own_components/categoryBlogView';
 import BookStore from './components/collection_components/bookStore';
 import ArticleStore from './components/collection_components/articleStore';
 import ApiTest from './components/collection_components/api_check';
@@ -16,6 +17,9 @@ const ContentManager = () => {
       </Route>
        <Route path="/Blog/:url" exact>
         <BlogView  />
+       </Route>
+      <Route path="/Category/:category" exact>
+        <CategoryBlogView  />
        </Route>
       <Route path="/News" >
         <ArticleStore />
