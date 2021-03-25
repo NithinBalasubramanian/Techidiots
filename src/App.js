e1d6cf3ba2bb3be5272e19a179819fd2b4ad5a4c
Nithin Migo <nithinmigo1@gmail.com>
2021-03-25 22:16:30 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <ContentManager />
         <Footer />
