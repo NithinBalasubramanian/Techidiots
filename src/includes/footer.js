6505c445713f170c6c6bf7950274a032a50804fb
Nithin <nithinmigocrogle22@outlook.com>
2021-03-30 15:15:28 +0530

    Update footer.js

--- src/includes/footer.js
+++ src/includes/footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
     let Year  = date.getFullYear();
     
     const subscribe = () => {
-        let emailSubscribe = email.current.value();
+        let emailSubscribe = email.current;
         alert(emailSubscribe);
     }
 
