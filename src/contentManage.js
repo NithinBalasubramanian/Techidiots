3b47d89a88122b33c7c777446d1196bf1c9fe14d
Nithin <nithinmigocrogle22@outlook.com>
2021-04-09 11:25:17 +0530

    Update contentManage.js

--- src/contentManage.js
+++ src/contentManage.js
@@ -3,8 +3,6 @@ import './style.scss';
 import HomeListoutComponent from './components/own_components/homeListoutComponent';
 import BlogView  from './components/own_components/blogView';
 import CategoryBlogView from './components/own_components/categoryBlogView';
-import BookStore from './components/collection_components/bookStore';
-import ArticleStore from './components/collection_components/articleStore';
 import ApiTest from './components/collection_components/api_check';
 import { Switch , Route } from 'react-router-dom';
 
@@ -21,15 +19,6 @@ const ContentManager = () => {
       <Route path="/Category/:category" exact>
         <CategoryBlogView />
        </Route>
-      <Route path="/News" >
-        <ArticleStore />
-      </Route>
-      <Route path="/Books" >
-        <BookStore />
-      </Route>
-      <Route path="/ApiTest" >
-        <ApiTest />
-      </Route>
     </Switch>
     </div>
   )
