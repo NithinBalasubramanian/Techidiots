8f4c517c3c6f35306fc6f5f9756ef157070056c8
Nithin <nithinmigocrogle22@outlook.com>
2021-04-09 11:23:31 +0530

    Update blogView.js

--- src/components/own_components/blogView.js
+++ src/components/own_components/blogView.js
@@ -74,13 +74,13 @@ const BlogView = () => {
                             <meta property="og:image" content="https://techidiots.in/favicon.jpg"/>
                             <meta property="og:url" content={ onUrl } />
                             <meta property="og:site_name" content={ 'www.'+onUrl }/>
-                            <meta property="og:type" content="website">
+                            <meta property="og:type" content="website" />
                             <meta property="og:title" content={ itm.title } />
                             <meta property="og:description" content={ itm.preheading } />
                             
                             <meta property="twitter:url" content={ onUrl } />
-                            <meta property="twitter:card" content="summary_large_image">
-                            <meta property="twitter:image" content="https://techidiots.in/favicon.jpg">
+                            <meta property="twitter:card" content="summary_large_image" />
+                            <meta property="twitter:image" content="https://techidiots.in/favicon.jpg" />
                             <meta property="twitter:title" content={ itm.title }  />
                             <meta property="twitter:description" content={ itm.preheading } />
                                 
