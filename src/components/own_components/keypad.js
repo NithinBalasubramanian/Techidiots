30b4247e65b52b6561f1d45d887e2816eaf018fb
Nithin <nithinmigocrogle22@outlook.com>
2021-05-18 22:02:09 +0530

    Update keypad.js

--- src/components/own_components/keypad.js
+++ src/components/own_components/keypad.js
@@ -4,71 +4,71 @@ import '../../keypad.css';
 const Keypad = () => {
   return(
     <div className="keypadBody">
-     <h1 class="title">Eyes on the screen</h1>
-        <div class="keyboard">
-          <ul class="row row-0">
-            <li class="pinky" id="esc">ESC</li>
-            <li class="pinky" id="1">1</li>
-            <li class="ring" id="2">2</li>
-            <li class="middle" id="3">3</li>
-            <li class="pointer1st" id="4">4</li>
-            <li class="pointer2nd" id="5">5</li>
-            <li class="pointer2nd" id="6">6</li>
-            <li class="pointer1st" id="7">7</li>
-            <li class="middle" id="8">8</li>
-            <li class="ring" id="9">9</li>
-            <li class="pinky" id="10">0</li>
-            <li class="pinky" >-</li>
-            <li class="pinky" >+</li>
-            <li class="pinky" id="back">BACK</li>
+     <h1 className="title">Eyes on the screen</h1>
+        <div className="keyboard">
+          <ul className="row row-0 keylist">
+            <li className="pinky" id="esc">ESC</li>
+            <li className="pinky" id="1">1</li>
+            <li className="ring" id="2">2</li>
+            <li className="middle" id="3">3</li>
+            <li className="pointer1st" id="4">4</li>
+            <li className="pointer2nd" id="5">5</li>
+            <li className="pointer2nd" id="6">6</li>
+            <li className="pointer1st" id="7">7</li>
+            <li className="middle" id="8">8</li>
+            <li className="ring" id="9">9</li>
+            <li className="pinky" id="10">0</li>
+            <li className="pinky" >-</li>
+            <li className="pinky" >+</li>
+            <li className="pinky" id="back">BACK</li>
           </ul>
-          <ul class="row row-1">
-            <li class="pinky" id="tab">TAB</li>
-            <li class="pinky" id="Q">Q</li>
-            <li class="ring" id="W">W</li>
-            <li class="middle" id="E">E</li>
-            <li class="pointer1st" id="R">R</li>
-            <li class="pointer2nd" id="T">T</li>
-            <li class="pointer2nd" id="Y">Y</li>
-            <li class="pointer1st" id="U">U</li>
-            <li class="middle" id="I">I</li>
-            <li class="ring" id="O">O</li>
-            <li class="pinky" id="P">P</li>
-            <li class="pinky" >[</li>
-            <li class="pinky" >]</li>
-            <li class="pinky" >\</li>
+          <ul className="row row-1 keylist">
+            <li className="pinky" id="tab">TAB</li>
+            <li className="pinky" id="Q">Q</li>
+            <li className="ring" id="W">W</li>
+            <li className="middle" id="E">E</li>
+            <li className="pointer1st" id="R">R</li>
+            <li className="pointer2nd" id="T">T</li>
+            <li className="pointer2nd" id="Y">Y</li>
+            <li className="pointer1st" id="U">U</li>
+            <li className="middle" id="I">I</li>
+            <li className="ring" id="O">O</li>
+            <li className="pinky" id="P">P</li>
+            <li className="pinky" >[</li>
+            <li className="pinky" >]</li>
+            <li className="pinky" >\</li>
           </ul>
-          <ul class="row row-2">
-            <li class="pinky" id="caps">CAPS</li>
-            <li class="pinky" id="A">A</li>
-            <li class="ring" id="S">S</li>
-            <li class="middle" id="D">D</li>
-            <li class="pointer1st" id="F">F</li>
-            <li class="pointer2nd" id="G">G</li>
-            <li class="pointer2nd" id="H">H</li>
-            <li class="pointer1st" id="J">J</li>
-            <li class="middle" id="K">K</li>
-            <li class="ring" id="L">L</li>
-            <li class="pinky" >:</li>
-            <li class="pinky" >''</li>
-            <li class="pinky" id="enter">ENTER</li>
+          <ul className="row row-2 keylist">
+            <li className="pinky" id="caps">CAPS</li>
+            <li className="pinky" id="A">A</li>
+            <li className="ring" id="S">S</li>
+            <li className="middle" id="D">D</li>
+            <li className="pointer1st" id="F">F</li>
+            <li className="pointer2nd" id="G">G</li>
+            <li className="pointer2nd" id="H">H</li>
+            <li className="pointer1st" id="J">J</li>
+            <li className="middle" id="K">K</li>
+            <li className="ring" id="L">L</li>
+            <li className="pinky" >:</li>
+            <li className="pinky" >''</li>
+            <li className="pinky" id="enter">ENTER</li>
           </ul>
-          <ul class="row row-3">
-            <li class="pinky" id="left-shift">SHIFT</li>
-            <li class="pinky" id="Z">Z</li>
-            <li class="ring" id="X">X</li>
-            <li class="middle" id="C">C</li>
-            <li class="pointer1st" id="V">V</li>
-            <li class="pointer2nd" id="B">B</li>
-            <li class="pointer2nd" id="N">N</li>
-            <li class="pointer1st" id="M">M</li>
-            <li class="middle" >,</li>
-            <li class="ring" >.</li>
-            <li class="pinky" >;</li>
-            <li class="pinky" id="right-shift">SHIFT</li>
+          <ul className="row row-3 keylist">
+            <li className="pinky" id="left-shift">SHIFT</li>
+            <li className="pinky" id="Z">Z</li>
+            <li className="ring" id="X">X</li>
+            <li className="middle" id="C">C</li>
+            <li className="pointer1st" id="V">V</li>
+            <li className="pointer2nd" id="B">B</li>
+            <li className="pointer2nd" id="N">N</li>
+            <li className="pointer1st" id="M">M</li>
+            <li className="middle" >,</li>
+            <li className="ring" >.</li>
+            <li className="pinky" >;</li>
+            <li className="pinky" id="right-shift">SHIFT</li>
           </ul>
         </div>
-        <h1 class="title">Hands on the keyboard</h1> 
+        <h1 className="title">Hands on the keyboard</h1> 
     </div>
   )
 }
